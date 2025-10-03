4145c590a68964c0f6f28a8ee83efb361c1b8fa4
ryosuke <20230175@tak.ed.jp>
2025-10-03 20:20:14 +0900

    app.py delete debug=True

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def sing():
     return render_template("sing.html", music_name=music_name)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
